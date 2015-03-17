d216cb7df01d2cf9c634289df955e6b6afd57218
Andrei Hodecker <andreih@outlook.com>
2015-03-16 21:10:05 -0300

    atualizando dist e arrumando erro no gulp

--- gulpfile.js
+++ gulpfile.js
@@ -39,11 +39,12 @@ gulp.task('html', ['styles'], function () {
 
 gulp.task('images', function () {
     return gulp.src('app/images/**/*')
-        .pipe($.cache($.imagemin({
+/*        .pipe($.cache($.imagemin({
             optimizationLevel: 3,
             progressive: true,
             interlaced: true
         })))
+*/
         .pipe(gulp.dest('dist/images'))
         .pipe($.size());
 });
